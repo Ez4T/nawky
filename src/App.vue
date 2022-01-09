e0b289ab354f7640c8c0ba28f25b53aa0b3fc7e9
Thee <tanarats7717@gmail.com>
2022-01-09 10:22:56 +0700

    push vue app

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="nav">
+    <div class="logo">
+      <div class="logo-img">
+        <img src="./assets/nawky.png" alt="nawky" />
+      </div>
+      <div class="logo-content">
+        <span class="main"><h1>NAWKY</h1></span>
+        <span class="sub"><p>NOT ACTIVE WE KICK YOU</p></span>
+      </div>
+    </div>
+    <div class="menu">
+      <div class="menu-content">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/leaderboard">Leaderboard</router-link>
+      </div>
+    </div>
+  </div>
+  <router-view />
+</template>
+
+<style>
+@font-face {
+  font-family: "Glacial-Indifference-Regular";
+  src: url("./fonts/glacial/Glacial-Indifference-Regular.ttf.woff")
+      format("woff"),
+    url("./fonts/glacial/Glacial-Indifference-Regular.ttf.svg#Glacial-Indifference-Regular")
+      format("svg"),
+    url("./fonts/glacial/Glacial-Indifference-Regular.ttf.eot"),
+    url("./fonts/glacial/Glacial-Indifference-Regular.ttf.eot?#iefix")
+      format("embedded-opentype");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "zing-rust-base";
+  src: url("./fonts/zing_rust/ZingRustG1-Trial-Base.woff") format("woff"),
+    url("./fonts/zing_rust/ZingRustG1-Trial-Base.woff2") format("woff2"),
+    url("./fonts/zing_rust/ZingRustG1-Trial-Base.eot"),
+    url("./fonts/zing_rust/ZingRustG1-Trial-Base.eot?#iefix")
+      format("embedded-opentype");
+  font-weight: normal;
+  font-style: italic;
+}
+
+h1 {
+  font-family: "Glacial-Indifference-Regular";
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: "Glacial-Indifference-Regular";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #19003c;
+}
+
+#nav a {
+  font-family: "Glacial-Indifference-Regular";
+  font-weight: bold;
+  color: #fff;
+  margin-right: 132px;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-size: 14pt;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.logo {
+  margin: 0 8.5rem;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+/* Start left-navbar */
+
+.logo-img > img {
+  width: 170px;
+}
+
+.logo-content {
+  margin-bottom: 20px;
+}
+
+.main > h1 {
+  margin-left: -55px;
+  font-family: "zing-rust-base";
+  color: white;
+  letter-spacing: 4px;
+  font-size: 36pt;
+}
+
+.sub > p {
+  font-family: "Glacial-Indifference-Regular";
+  color: white;
+  font-size: 13pt;
+  margin-top: -40px;
+  margin-left: -25px;
+}
+
+/* End loft-navbar */
+/* Start right-navbar */
+.menu {
+  margin: 0 1.5rem;
+  padding: 1rem 1rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="home">
+    <div class="home-content">
+      <div class="head">
+        <h1 class="welcome">
+          WELCOME TO<span class="the"><h4>THE</h4></span>
+        </h1>
+        <span class="nawky"><h1>NAWKY</h1></span>
+      </div>
+      <div class="sub">
+        <p>
+          NAWKY Club is a group of people who want to improve themselves in
+          various fileds. Let's share experiences and opinions!
+        </p>
+      </div>
+      <div class="button">
+        <button class="discord" @click="inviteLink">
+          <img src="../assets/discord-seeklogo.com.svg" alt="" />
+        </button>
+        <router-link to="/leaderboard" class="link">
+          <button class="leaderboard"><p>LEADERBOARD</p></button>
+        </router-link>
+      </div>
+    </div>
+    <div class="home-image">
+      <img src="../assets/content_1.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  methods: {
+    inviteLink () {
+      window.open('https://discord.gg/nEhfgdY2U6', '_blank')
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@800&display=swap");
+.home {
+  background-color: #19003c;
+  display: flex;
+  width: 100%;
+  height: 82.1vh;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.home-content {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 600px;
+}
+
+.head {
+  position: inline-box;
+}
+
+.welcome,
+.the,
+.nawky {
+  font-family: "Glacial-Indifference-Regular";
+}
+
+.welcome {
+  font-family: "Libre Franklin", sans-serif;
+  font-weight: bold;
+  color: #fff;
+  font-size: 40pt;
+}
+
+.the > h4 {
+  font-family: "Libre Franklin", sans-serif;
+  margin: -5px 270px 0px 0px;
+  font-size: 20pt;
+}
+
+.nawky > h1 {
+  font-family: "Libre Franklin", sans-serif;
+  font-weight: bold;
+  font-size: 46pt;
+  color: #19003c;
+  -webkit-text-stroke: 1px white;
+  letter-spacing: 5px;
+  margin-left: 70px;
+  margin-top: -75px;
+}
+
+.sub {
+  width: 400px;
+  text-align: left;
+  margin-top: 2rem;
+  margin-left: 140px;
+}
+
+.sub > p {
+  font-size: 16pt;
+}
+
+.home-image > img {
+  width: 911px;
+}
+
+.button {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 0 4rem;
+}
+
+.discord,
+.leaderboard {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
+  height: 40px;
+  border-radius: 1rem;
+}
+
+.discord {
+  background-color: transparent;
+  border: 2px solid white;
+  color: white;
+  font-family: "Libre Franklin", sans-serif;
+  cursor: pointer;
+}
+
+.discord > img {
+  height: 50px;
+}
+
+.leaderboard {
+  background-color: white;
+  color: #19003c;
+  font-family: "Libre Franklin", sans-serif;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.leaderboard > p {
+  font-size: 11pt;
+}
+
+.link{
+  text-decoration: none;
+}
+</style>
