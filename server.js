c8273acdb2bd106f661adb96999aa05f97a5b8fc
Thee <tanarats7717@gmail.com>
2022-01-09 14:11:45 +0700

    deleted eslint

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`app is listening on port : ${port}`)
+})
